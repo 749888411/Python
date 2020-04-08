65190d87b6dbe8df36472532aa2d2624bcd59629
749888411 <44027342+749888411@users.noreply.github.com>
2020-04-08 23:22:23 +0800

    Update http_post.py

--- http_post.py
+++ http_post.py
@@ -18,3 +18,27 @@ for key, value in a.items():
     print(key)
     print(res.content)
     time.sleep(1)
+
+
+# coding:utf-8
+import requests
+
+url = "https://passport.cnblogs.com/user/signin"
+# 接口地址
+# 消息头数据
+headers = {'Connection': 'keep-alive',
+           'Content-Length': '123',
+           'Cache-Control': 'max-age=0',
+           'Origin': 'https://passport.csdn.net',
+           'Upgrade-Insecure-Requests': '1',
+           'Content-Type': 'application/x-www-form-urlencoded',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+           'Referer': 'https://passport.csdn.net/account/login?from=http://www.csdn.net',
+           'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'zh-CN,zh;q=0.9',
+           'Cookie': '省略', }
+payload = {'username': '**', 'password': '**', 'lt': '**', 'execution': 'e1s1', '_eventId': 'submit', }
+# verify = False 忽略SSH 验证
+r = requests.post(url, json=payload, headers=headers, verify=False)
+json2 = r.json()
+print(json2)
